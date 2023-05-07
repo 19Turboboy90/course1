797af86232de8dd16152c003fe8edbe8fb3c050c
Ilia Zharinov <19turboboy90@gmail.com>
2023-05-07 21:52:53 +0300

    Added new project

---
+++ src/main/java/ru/zharinov/introduced/hw/FamilyMember.java
@@ -0,0 +1,15 @@
+package ru.zharinov.introduced.hw;
+
+/*
+Создать новый класс, который будет выводить имена членов вашей семьи, каждое с новой строки.
+Придумать классу соответствующее название
+ */
+public class FamilyMember {
+    public static void main(String[] args) {
+        System.out.println("Дедка");
+        System.out.println("Бабка");
+        System.out.println("Внучка");
+        System.out.println("Жучка");
+        System.out.println("Мурка");
+    }
+}
