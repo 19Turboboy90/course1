ba274fc61034273a545dab54a908e6be9b94d38f
Ilia Zharinov <19turboboy90@gmail.com>
2023-05-09 23:14:49 +0300

    Added themes arrays

---
+++ src/main/java/ru/zharinov/javacourse1/syntaxjava/remainingtopics/Space.java
@@ -0,0 +1,87 @@
+package ru.zharinov.javacourse1.syntaxjava.remainingtopics;
+
+/*
+Задача-1.
+
+Создать строковый массив с названиями месяцев, проинициализировать сразу.
+После этого вывести все названия месяцев в одну строчку и через запятую, а после последнего слова должна стоять точка.
+ */
+/*
+Задача-2
+
+Написать программу, которая принимает номер месяца и выводит его название на экран,
+если такого месяца нет, то вывести надпись "месяц не существует".
+ */
+public class Space {
+    public static void main(String[] args) {
+        //Задача-1.
+        //Мое решение
+//        String[] month = {"Январь", "Февраль", "Март", "Апрель", "Мая", "Июнь", "Июль",
+//                "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+//        for (int i = 0; i < month.length; i++) {
+//            System.out.print(month[i]);
+//            if (i == month.length - 1) {
+//                System.out.print(".");
+//            } else {
+//                System.out.print(", ");
+//            }
+//        }
+//        //решение автора
+//        String[] namesOfMonth = {"Январь", "Февраль", "Март", "Апрель", "Мая", "Июнь", "Июль",
+//                "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+//        String result = "";
+//        for (int i = 0; i < namesOfMonth.length; i++) {
+//            result += namesOfMonth[i];
+//            if (i == namesOfMonth.length - 1) {
+//                result += ".";
+//            } else {
+//                result += ", ";
+//            }
+//        }
+//        System.out.println(result);
+        //Задача-2.
+
+        int numberOfMonth = 1;
+        switch (numberOfMonth) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Мая");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+            default:
+                System.out.println("месяц не существует");
+                break;
+        }
+    }
+}
---
+++ src/main/java/ru/zharinov/javacourse1/syntaxjava/remainingtopics/hw/NumberOgMonth.java
@@ -0,0 +1,38 @@
+package ru.zharinov.javacourse1.syntaxjava.remainingtopics.hw;
+
+/*
+Написать программу, которая принимает строковую переменную с названием месяца. И если месяц введен правильно,
+то вывести на экран время года, к которому этот месяц относится, в противном случае вывести сообщение, что месяц не существует.
+
+Выполнить сначала при помощи if-else, а затем при помощи switch-case
+ */
+public class NumberOgMonth {
+    public static void main(String[] args) {
+        String nameOfMonth = "Май";
+        switch (nameOfMonth) {
+            case "Декабрь":
+            case "Январь":
+            case "Февраль":
+                System.out.println("Зима");
+                break;
+            case "Март":
+            case "Апрель":
+            case "Май":
+                System.out.println("Весна");
+                break;
+            case "Июнь":
+            case "Июль":
+            case "Август":
+                System.out.println("Лето");
+                break;
+            case "Сентябрь":
+            case "Октябрь":
+            case "Ноябрь":
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Месяца не существует");
+
+        }
+    }
+}
