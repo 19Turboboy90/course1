fae7ecc9381f3f2025d86b5c1f1cc5beafe8c95f
Ilia Zharinov <19turboboy90@gmail.com>
2023-05-14 22:37:22 +0300

    Added new themes

---
+++ src/main/java/ru/zharinov/javacourse1/oop/own_collection/MyArrayList.java
@@ -0,0 +1,48 @@
+package ru.zharinov.javacourse1.oop.own_collection;
+
+public class MyArrayList {
+    private String[] array = new String[10];
+    private int size = 0;
+
+    public void add(String s) {
+        array[size] = s;
+        size++;
+        if (size == array.length) {
+            String[] newArray = new String[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+    }
+
+    public void remove(int index) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < array.length - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+    }
+
+    public void remove(String s) {
+        for (int i = 0; i < array.length; i++) {
+            if (s.equals(array[i])) {
+                remove(i);
+                break;
+            }
+        }
+    }
+
+    public String get(int index) {
+        if (index >= 0) {
+            return array[index];
+        } else {
+            return "Объект не найден";
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/main/java/ru/zharinov/javacourse1/oop/static_final/Main.java
@@ -0,0 +1,15 @@
+package ru.zharinov.javacourse1.oop.static_final;
+
+public class Main {
+    public static void main(String[] args) {
+        int square = MyMath.square(20);
+        double length = MyMath.length(10);
+        double area = MyMath.area(10);
+        int sum = MyMath.sum(1, 4);
+        System.out.println(sum);
+        System.out.println(square);
+        System.out.println(area);
+        System.out.println(length);
+        System.out.println(square);
+    }
+}
